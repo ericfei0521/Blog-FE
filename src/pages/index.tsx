1febb6ec9a69d38d2d0a9f673b7022b01ea763aa
ericfei306 <eric0521bigjohn@gmail.com>
2024-06-08 00:42:21 +0800

    add pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import type { InferGetStaticPropsType } from 'next'
+import type { InferGetStaticPropsType } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
-const  Home = ({result}:InferGetStaticPropsType<typeof getStaticProps>)=>{
-  console.log('result',result)
+const Home = ({ result }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log("result", result);
   return (
     <>
       <Head>
@@ -11,17 +12,20 @@ const  Home = ({result}:InferGetStaticPropsType<typeof getStaticProps>)=>{
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
+      <main>
         <h1>hello</h1>
+        <Link href="/new-post">
+          <button>+New Post</button>
+        </Link>
       </main>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
 
-export const getStaticProps = (async ()=>{
-  const res = await  fetch("https://catfact.ninja/fact")
-  const result = await res.json()
-  return {props:{result}}
-})
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:8080/posts");
+  const result = await res.json();
+  return { props: { result } };
+};

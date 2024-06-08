77f621cb1ae64ed45f09510026b4493e165b512b
Eric Fei <eric.fei@partipost.com>
2024-06-08 16:24:33 +0800

    update lint set up

---
+++ src/pages/newPost.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import { json } from "stream/consumers"
+
+const NewPost = () => {
+    const [data, setData] = useState<{
+        title: string
+        content: string
+        image: string | null
+    }>({
+        title: "",
+        content: "",
+        image: null,
+    })
+
+    const upload = () => {
+        console.log(data)
+        fetch("http://localhost:8080/posts/create-post", {
+            body: JSON.stringify(data),
+            headers: {
+                "content-type": "application/json",
+            },
+            method: "post",
+        })
+            .then((res) => console.log("res"))
+            .catch((err) => console.log(err))
+    }
+
+    return (
+        <div>
+            <h1>New Post</h1>
+            <input
+                type="text"
+                name="title"
+                onChange={(e) => {
+                    setData({ ...data, title: e.target.value })
+                }}
+            />
+            <textarea
+                name="content"
+                rows={3}
+                onChange={(e) => {
+                    setData({ ...data, content: e.target.value })
+                }}
+            />
+            <input
+                type="file"
+                name="image"
+                onChange={(e) => {
+                    setData({ ...data, image: e.target.value })
+                }}
+            />
+            <button onClick={upload}>Submit</button>
+        </div>
+    )
+}
+
+export default NewPost
